36a86b202da203684be9113ecb14535566cfd7e6
fedeloxe <loxeee@gmail.com>
2022-11-26 04:21:22 +0100

    css added

---
+++ bonus/js/script.js
@@ -0,0 +1,39 @@
+//prompt km
+let km = prompt ('Quanti km devi percorrere?')
+
+console.log (km)
+
+//prompt età 
+
+let eta = prompt ('inserisci la tua età')
+
+console.log(eta)
+
+//calcolo prezzo
+
+let firstprice = km * '0.21'
+
+console.log(firstprice)
+
+//applico sconti 
+if(eta < 18){
+    let discountvalue = (firstprice / 100) *20;
+    let finalPrice = (firstprice - discountvalue).toFixed(2) ;
+    console.log(finalPrice)
+    document.getElementById ("price").innerHTML = finalPrice ;
+}
+
+else if (eta > 65){
+    let discountvalue = (firstprice / 100) *40;
+    let finalPrice = (firstprice - discountvalue).toFixed(2);
+    console.log(finalPrice)
+    document.getElementById ("price").innerHTML = finalPrice ;
+}
+
+else{
+    let finalPrice = firstprice
+    document.getElementById ("price").innerHTML = finalPrice;
+}
+
+
+
